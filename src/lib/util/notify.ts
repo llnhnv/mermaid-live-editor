960abb54c1376589fea54da6876e909370a81421
Vu-Linh Nguyen <NguyenVuLinh99@gmail.com>
2026-03-15 01:35:34 +0700

    feat: add success and error notification functions

--- src/lib/util/notify.ts
+++ src/lib/util/notify.ts
@@ -4,6 +4,14 @@ export const notify = (message: string): void => {
   toast(message);
 };
 
+export const notifySuccess = (message: string): void => {
+  toast.success(message);
+};
+
+export const notifyError = (message: string): void => {
+  toast.error(message);
+};
+
 export const prompt = (message: string): boolean => {
   return confirm(message);
 };
